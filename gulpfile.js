baff652deed3ea0d2f1b47b5228f6b2a21009ecd
KingPra <kingi.pra@gmail.com>
2017-05-13 18:18:47 -0400

    updated gulpfile, added strip-comments

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,46 @@
  * that convert from 'developer mode' to 'productioin mode'. Things
  * like converting Sass => CSS, removing comments, merging
  * files, etc
-  */
+ */
 
-  // Step 1: import gulp
-  let gulp = require('gulp');
-  let sass = require('gulp-sass');
-  let browser = require('gulp-browser');
+// Step 1: import gulp
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browser = require('gulp-browser');
+const strip = require('gulp-strip-comments');
 
-  // Step 2: create default tasks
-  gulp.task('default', ['html', 'css', 'js']);
+// Step 2: create default tasks
+gulp.task('default', ['html', 'css', 'js']);
 
-  // Step 3: create subtasks
-  gulp.task('html', function () {
-      return gulp.src('index.html')
-        .pipe(gulp.dest('public/'));
-  });
+// Step 3: create subtasks
+gulp.task('html', () => {
+  return gulp.src('index.html')
+    .pipe(strip())
+    .pipe(gulp.dest('docs/'));
+});
 
-  gulp.task('css', function () {
-      // Convert style.scss into style.css
-      // Copy to public/ 
-      return gulp.src('scss/style.scss')
-        .pipe(sass()) // requires gulp-sass
-        .pipe(gulp.dest('public/'));
-  });
+gulp.task('css', () => {
+  // Convert style.scss into style.css
+  // Copy to public/
+  return gulp.src('scss/style.scss')
+    .pipe(sass()) // requires gulp-sass
+    .pipe(strip.text())
+    .pipe(gulp.dest('docs/'));
+});
 
-  gulp.task('js', function () {
-      // Copy js file into public/
-      return gulp.src('js/app.js')
-        .pipe(browser.browserify()) // makes require work
-        .pipe(gulp.dest('public/'));
-  });
+gulp.task('js', () => {
+  // Copy js file into public/
+  return gulp.src('js/app.js')
+    .pipe(strip())
+    .pipe(browser.browserify()) // makes require work
+    .pipe(gulp.dest('docs/'));
+});
 
-  gulp.task('watch', ['default'], function () {
-    // gulp.watch('files-to-watch', 'tasks-to-run')
-    //gulp.watch('index.html',['html']); //watches a specific file 'index.html'
-   gulp.watch('*.html',['html']); //watches all html files
-   gulp.watch('scss/*.scss', ['css']);
-   gulp.watch('js/*/*.js', ['js']);
-   gulp.watch('js/*.js', ['js']);
+gulp.task('watch', ['default'], function() {
+  // gulp.watch('files-to-watch', 'tasks-to-run')
+  //gulp.watch('index.html',['html']); //watches a specific file 'index.html'
+  gulp.watch('*.html', ['html']); //watches all html files
+  gulp.watch('scss/*.scss', ['css']);
+  gulp.watch('js/*/*.js', ['js']);
+  gulp.watch('js/*.js', ['js']);
 });
